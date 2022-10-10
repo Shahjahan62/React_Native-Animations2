90c42c2eed95b840eb947a83af2399c1f39502d2
Shah Jahan <95079845+Shahjahan62@users.noreply.github.com>
2022-10-10 12:26:16 +0500

    DEcay animations

---
+++ src/DecayAnimate.tsx
@@ -0,0 +1,12 @@
+import { View, Text, Animated } from "react-native";
+import React from "react";
+
+const DecayAnimate = () => {
+  return (
+    <View>
+      <Animated.View />
+    </View>
+  );
+};
+
+export default DecayAnimate;
