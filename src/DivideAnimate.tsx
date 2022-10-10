a7cc299fa4ea542a69babfde9823f685e37f33a0
Shah Jahan <95079845+Shahjahan62@users.noreply.github.com>
2022-10-10 13:26:30 +0500

    divide animations

--- src/DivideAnimate.tsx
+++ src/DivideAnimate.tsx
@@ -1,10 +1,11 @@
-import { View, Text } from "react-native";
+import { View, Text, Animated } from "react-native";
 import React from "react";
 
 const DivideAnimate = () => {
+  const animated = new Animated.Value(0);
   return (
     <View>
-      <Text>DivideAnimate</Text>
+      <Text>This is new commit</Text>
     </View>
   );
 };
