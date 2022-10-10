071056a62de858e0d568e691745fe8be06cb9b9e
Shah Jahan <95079845+Shahjahan62@users.noreply.github.com>
2022-10-10 12:44:02 +0500

    decay aniamtion

--- src/DecayAnimate.tsx
+++ src/DecayAnimate.tsx
@@ -1,10 +1,43 @@
-import { View, Text, Animated } from "react-native";
-import React from "react";
+import { View, Text, Animated, PanResponder } from "react-native";
+import React, { Component } from "react";
 
 const DecayAnimate = () => {
+  const animated: any = new Animated.ValueXY(0);
+
+  function ComponentWillMount() {
+    var panRasponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onMoveShouldSetPanResponder: () => true,
+      onPanResponderMove: Animated.event([
+        null,
+        {
+          dx: animated.x,
+          dy: animated.y,
+        },
+      ]),
+      onPanResponderRelease: (e, { vx, vy }) => {
+        Animated.decay(animated, {
+          velocity: { x: vx, y: vy },
+          deceleration: 0997,
+          useNativeDriver: false,
+        }).start();
+      },
+    });
+  }
+
+  const animatedStyle = {
+    transform: animated.getTranslateTransform(),
+  };
+
   return (
     <View>
-      <Animated.View />
+      <Animated.View
+        style={[
+          { width: 60, height: 60, backgroundColor: "teal" },
+          animatedStyle,
+        ]}
+        {...panResponder.panHandlers}
+      />
     </View>
   );
 };
