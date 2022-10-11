3ffccf7e667b52a8c49c68490acee850f7ba4d78
Shah Jahan <95079845+Shahjahan62@users.noreply.github.com>
2022-10-11 10:25:32 +0500

    Delay animation

--- App.tsx
+++ App.tsx
@@ -12,6 +12,7 @@ import ModuloAnimate from "./src/ModuloAnimate";
 import ParallelAnimate from "./src/Combining Animations/ParallelAnimate";
 import SecuenceAnimate from "./src/Combining Animations/SecuenceAnimate";
 import StaggerAnimate from "./src/Combining Animations/StaggerAnimate";
+import DelayAnimate from "./src/Combining Animations/DelayAnimate";
 export default function App() {
   return (
     // <PanPonder />
@@ -25,7 +26,8 @@ export default function App() {
     // <ModuloAnimate />
     // <ParallelAnimate />
     // <SecuenceAnimate />
-    <StaggerAnimate />
+    // <StaggerAnimate />
+    <DelayAnimate />
   );
 }
 
