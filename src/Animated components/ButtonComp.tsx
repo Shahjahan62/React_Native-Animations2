c352988a931b7ba7f19e084a33a64a9cec58214e
Shah Jahan <95079845+Shahjahan62@users.noreply.github.com>
2022-10-12 12:18:58 +0500

    button comp

---
+++ src/Animated components/ButtonComp.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const ButtonComp = () => {
+  return (
+    <View>
+      <Text>ButtomComp</Text>
+    </View>
+  );
+};
+
+export default ButtonComp;
+
+const styles = StyleSheet.create({});
