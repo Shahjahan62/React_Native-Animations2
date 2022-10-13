b7a1d597e1b40e5771d5e1baffa298d9e10666ae
Shah Jahan <95079845+Shahjahan62@users.noreply.github.com>
2022-10-13 14:02:20 +0500

    animation

--- src/AllFunctions/AllFunctions.tsx
+++ src/AllFunctions/AllFunctions.tsx
@@ -1,11 +1,58 @@
-import { StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 import React from "react";
-
+import AddAnimate from "../AddAnimted";
+import DecayAnimate from "../DecayAnimate";
+import DivideAnimate from "../DivideAnimate";
+import ModuloAnimate from "../ModuloAnimate";
 const AllFunctions = () => {
   return (
-    <View>
-      <Text>AllFunctions</Text>
-    </View>
+    <ScrollView>
+      <View
+        style={{
+          alignItems: "center",
+          height: 450,
+          backgroundColor: "red",
+          paddingTop: 30,
+
+          //   flex: 1,
+        }}
+      >
+        <Text style={{ fontWeight: "900", fontSize: 30 }}>
+          Add function Animation{" "}
+        </Text>
+        <AddAnimate />
+      </View>
+      <View
+        style={{
+          alignItems: "center",
+          height: 450,
+          backgroundColor: "lime",
+          paddingTop: 30,
+
+          //   flex: 1,
+        }}
+      >
+        <Text style={{ fontWeight: "900", fontSize: 30 }}>
+          Division function Animation{" "}
+        </Text>
+        <DivideAnimate />
+      </View>
+      <View
+        style={{
+          alignItems: "center",
+          height: 450,
+          backgroundColor: "#50C878",
+          paddingTop: 30,
+
+          //   flex: 1,
+        }}
+      >
+        <Text style={{ fontWeight: "900", fontSize: 30 }}>
+          Modulo function Animation{" "}
+        </Text>
+        <ModuloAnimate />
+      </View>
+    </ScrollView>
   );
 };
 
