3eb01fdffcdaa80eddcafa72a9d8106a284850a1
Shah Jahan <95079845+Shahjahan62@users.noreply.github.com>
2022-10-13 11:42:56 +0500

    All Animations functions

---
+++ src/AllFunctions/AllFunctions.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const AllFunctions = () => {
+  return (
+    <View>
+      <Text>AllFunctions</Text>
+    </View>
+  );
+};
+
+export default AllFunctions;
+
+const styles = StyleSheet.create({});
